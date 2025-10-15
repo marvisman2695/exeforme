83894eff65a60d5b3d6321040cc46a741d4de020
marvisman2695 <marvisman2695@gmail.com>
2025-10-16 00:55:31 +0800

    update

---
+++ practice.java
@@ -0,0 +1,42 @@
+public class practice {
+  public static void main (String[] args){
+    int a = 8;
+    if (a> 4) {
+System.out.println("Happy!");
+    } else {
+System.out.println("NO!");
+}
+    int b = 13;
+if (a > 4 && b > 12) {
+  System.out.println("abcde");
+  } else {
+    System.out.println("ijkl");
+  }
+
+if (a > 6 && b > 8) {
+  System.out.println("Ya");
+} else {
+  System.out.println("ok!");
+}
+
+
+//String + double
+String s1 = "Louis" + 1.1;
+System.out.println(s1);
+
+//int + int = int
+// int + double = double
+// double + double = double
+// double + int = double
+
+//length
+String n1 = "fabulous";
+System.out.println(n1.length());
+System.out.println(n1.charAt(0));
+System.out.println(n1.isEmpty());
+
+
+  }
+
+
+}
